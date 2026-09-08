5195ad35f5f7f696e06b8d895c457cc8e9b4e6ae
fizzboy <onihime1989@gmail.com>
2026-09-08 12:47:36 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 export const APP_VERSION="2.6.3";
-export const SUPABASE_URL="https://vfiansclxywtbkljtbdl.supabase.co/rest/v1/";
+export const SUPABASE_URL="https://vfiansclxywtbkljtbdl.supabase.co";
 export const SUPABASE_ANON_KEY="sb_publishable_25M_MHBfQtUFADNL1c-8DQ_2erFaG_C";
