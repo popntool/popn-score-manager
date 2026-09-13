3d15ca5b86634968612115d62508b1021b772c54
fizzboy <onihime1989@gmail.com>
2026-09-13 21:03:18 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,3 +1,2 @@
-export const APP_VERSION="2.6.3";
 export const SUPABASE_URL="https://vfiansclxywtbkljtbdl.supabase.co";
 export const SUPABASE_ANON_KEY="sb_publishable_25M_MHBfQtUFADNL1c-8DQ_2erFaG_C";
