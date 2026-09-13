02c0e1d3b43c477f9cae60e44239ee398ebe8600
fizzboy <onihime1989@gmail.com>
2026-09-13 22:21:35 +0900

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -1,5 +1,5 @@
 import{popClassSelection,songPopClass}from"./scores.js?v=3.0.88";
-import{medalInfo}from"./ui.js?v=3.0.96";
+import{medalInfo}from"./ui.js?v=3.0.97";
 
 const HASHTAG="#popn_score_manager",SHARE_TEXT=`${HASHTAG}\n`,BG="#fffaf0",PANEL="#fffdf6",INK="#142b67",MUTED="#69789d",LINE="#3153a0",ACCENT="#ffd851",PINK="#ff789a";
 const TARGET_BYTES=1024*1024;
